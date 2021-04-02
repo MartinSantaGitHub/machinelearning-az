6bea410cc499152da7b814e25a3e726a1878947e
msantamaria <martin_e5@hotmail.com>
2021-04-02 19:52:06 -0300

    Update

--- codigo/09 - Dimensionality Reduction/1 - Principal Component Analysis (PCA)/pca.py
+++ codigo/09 - Dimensionality Reduction/1 - Principal Component Analysis (PCA)/pca.py
@@ -32,6 +32,7 @@ from sklearn.decomposition import PCA
 pca = PCA(n_components = 2)
 X_train = pca.fit_transform(X_train)
 X_test = pca.transform(X_test)
+
 explained_variance = pca.explained_variance_ratio_
 
 # Ajustar el modelo de Regresión Logística en el Conjunto de Entrenamiento
